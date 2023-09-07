6a78b858786554b23179372d13b404c3d5ba0dbf
Ujjawal-Chaurasia <chaurasiaujjawal208@gmail.com>
2023-09-07 16:46:55 +0530

    removing build hosting codes from appjs

--- app.js
+++ app.js
@@ -3,8 +3,8 @@ import  cors from  'cors';
 import  bodyParser   from "body-parser";
 //Route Imports
 import todosRoute from './routes/todo.js'
-import path from 'path'
-const __dirname=path.resolve();
+// import path from 'path'
+// const __dirname=path.resolve();
 
 const app=express();
 
@@ -19,12 +19,10 @@ app.use(bodyParser.urlencoded({extended:true}));
 
 app.use("/api/v1",todosRoute);
 
-app.use(express.static(path.join(__dirname,"./frontend/build")))
-app.get("*",(req,res)=>{
-    res.sendFile(path.resolve(__dirname,"./frontend/build/index.html"))
-})
+// app.use(express.static(path.join(__dirname,"./frontend/build")))
+// app.get("*",(req,res)=>{
+//     res.sendFile(path.resolve(__dirname,"./frontend/build/index.html"))
+// })
 
-//MiddleWare for error
-// app.use(errorMiddleware)
 
 export default app;
